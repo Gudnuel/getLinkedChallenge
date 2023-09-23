a3e30aeed171640651e102a1a71fc3963750e2c1
Emmanuel <obadakiemmanuel@gmail.com>
2023-09-23 12:08:42 +0100

    fixing deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
-import { MainPage, RegisterPage, ContactPage } from "./pages/index";
-
+import {
+  MainPage,
+  RegisterPage,
+  ContactPage,
+  NotFoundPage,
+} from "./pages/index";
 import NavBar from "./components/header/NavBar";
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </>
       </div>
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <H1>Page Not Found</H1>
+      <Link to="/"></Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
